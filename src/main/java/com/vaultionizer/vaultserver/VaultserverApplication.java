3dd363f3ed90a3a586c37fe0d3aa7dc2003cf4bc
Julien Meier <keksklauer4@gmail.com>
2020-10-06 10:32:38 +0200

    Initial commit.

---
+++ src/main/java/com/vaultionizer/vaultserver/VaultserverApplication.java
@@ -0,0 +1,13 @@
+package com.vaultionizer.vaultserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vaultionizer/vaultserver/VaultserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaultionizer.vaultserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
