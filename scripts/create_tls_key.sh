f395c10b2b52740e45fc7f54cd15fb610a9a9117
Julien Meier <keksklauer4@gmail.com>
2020-11-03 12:40:14 +0100

    Changed tls script to letsencrypt.

--- scripts/create_tls_key.sh
+++ scripts/create_tls_key.sh
@@ -1,9 +1,15 @@
+DOMAIN_NAME=abc.de
+
 cd ..
 
 rm vaultionizerServer.p12
 rm src/main/resources/keystore/vaultionizerServer.p12
 
-keytool -genkeypair -alias vaultionizerServer -keyalg RSA -keysize 2048 -storetype PKCS12 -keystore vaultionizerServer.p12 -validity 3650
+git clone https://github.com/certbot/certbot
+cd certbot
+sudo ./certbot-auto certonly -a standalone -d $DOMAIN_NAME -d www.$DOMAIN_NAME
+
+openssl pkcs12 -export -in /etc/letsencrypt/live/$DOMAIN_NAME/fullchain.pem -inkey /etc/letsencrypt/live/$DOMAIN_NAME/privkey.pem -out vaultionizerServer.p12 -name vaultionizerServer -CAfile chain.pem -caname root
 
 mkdir src/main/resources/keystore
 cp vaultionizerServer.p12 src/main/resources/keystore/vaultionizerServer.p12
