b82f0f69d3a6795f230c8d1502340fd39b13e770
Julien Meier <keksklauer4@gmail.com>
2020-11-21 19:41:05 +0100

    Fixed ref file too long bug.

--- src/main/java/com/vaultionizer/vaultserver/model/db/RefFilesModel.java
+++ src/main/java/com/vaultionizer/vaultserver/model/db/RefFilesModel.java
@@ -1,9 +1,6 @@
 package com.vaultionizer.vaultserver.model.db;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
@@ -24,6 +21,7 @@ public class RefFilesModel {
     @PastOrPresent(message = "Last time updated content cannot possibly be in the future!")
     private Instant lastUpdatedContent; // needed to specify when the ref file has been updated the last time
 
+    @Column(columnDefinition = "TEXT")
     @NotNull(message = "Reference file's content cannot be null!")
     private String content;
 
